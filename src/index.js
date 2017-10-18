a0a0e472cf7f5050d4f3189c9d75b04f1a519304
kryseno <kryseno@gmail.com>
2017-10-18 00:04:15 -0700

    completed FeatureSet1 & up to FeatureSet 2.2

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ ReactDOM.render(
     <h1 className="container">React Prototypes!</h1>,
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    return Math.floor((Math.random() * 1000) + 1);
+}
+
+function Greeting(user){
+    return <div className="container">
+        <h1>Hello, {user.name}!</h1>
+        <h2 className="text-muted">Your lucky number is: {user.luckyNum}</h2>
+    </div>
+}
