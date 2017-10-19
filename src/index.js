cccd952c1ba5e782e68ccc8f3ea1144ac1fe86b2
kryseno <kryseno@gmail.com>
2017-10-19 16:20:33 -0700

    React Intro - Krystal Enojado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj = {
+    name: 'krys',
+    luckyNum: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
 
@@ -10,7 +15,7 @@ function luckyNumber(){
     return Math.floor((Math.random() * 1000) + 1);
 }
 
-function Greeting(user){
+function greeting(user){
     return <div className="container">
         <h1>Hello, {user.name}!</h1>
         <h2 className="text-muted">Your lucky number is: {user.luckyNum}</h2>
