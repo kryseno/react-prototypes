5482130c665ec5dd50d5fdfdb323fd32cc4dd903
kryseno <kryseno@gmail.com>
2017-10-23 18:42:57 -0700

    completed FeatureSet 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Stopwatch from '../components/stopwatch';
 
 export default function App(props){
     return (
         <div className="container">
             <h1 className="text-center">Stopwatch</h1>
+            <Stopwatch/>
         </div>
     )
 }
--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,3 +1,14 @@
-inport React, { Component } from 'react';
+import React, { Component } from 'react';
 
-class Stopwatch extends Component
+class Stopwatch extends Component {
+
+    render(){
+        return (
+            <div>
+                <h3>This will be the stopwatch</h3>
+            </div>
+        )
+    }
+}
+
+export default Stopwatch;
