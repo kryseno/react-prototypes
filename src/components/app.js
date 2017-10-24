2d5ae19cc15ffeffc03d8894a7679481c87a4e5e
kryseno <kryseno@gmail.com>
2017-10-23 20:00:44 -0700

    completed FeatureSet 1

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,9 @@
+import React from 'react';
 
-
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center my-3">Address Book</h1>
+        </div>
+    )
+}
